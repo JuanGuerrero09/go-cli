c4921c6c063d96e54d5750fc6e6ba24295c7737a
juanguerrero09 <juanguerrero09mc@gmail.com>
2024-01-31 00:47:34 +0100

    add filter out

--- 04-cli/walk/actions.go
+++ 04-cli/walk/actions.go
@@ -7,6 +7,12 @@ import (
 	"path/filepath"
 )
 
-func filterOut(path, ext string, minSize int64, info os.FileInfo){
-
+func filterOut(path, ext string, minSize int64, info os.FileInfo) bool {
+	if info.IsDir() || info.Size() < minSize {
+		return true
+	}
+	if ext != "" && filepath.Ext(path) != ext {
+		return true
+	}
+	return false
 }
